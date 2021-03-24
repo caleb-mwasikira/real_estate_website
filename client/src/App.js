ed5e1b9f66bfeb1939b6287ea2f019535c1212af
Caleb-Mwasikira <calebmwasikira@gmail.com>
2021-03-24 13:36:27 +0300

    Created a Login and SignUp Page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import './App.css';
 
-import LandingPage from "./pages/LandingPage";
+// import LandingPage from "./pages/LandingPage";
+import SignUpPage from "./pages/SignUpPage";
 
 
 class App extends React.Component {
   render() {
     return(
       <div className="app">
-        <LandingPage />
+        {/* <LandingPage /> */}
+
+        <SignUpPage />
       </div>
     );
   }
---
+++ client/src/pages/SignUpPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FormCard from '../components/forms/FormCard';
+
+
+class SignUpPage extends Component {
+    render() {
+        return (
+            <div className="signup_page">
+                <FormCard />
+            </div>
+        )
+    }
+}
+
+
+export default SignUpPage;
